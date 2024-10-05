b65d07ebf317d48aaef4f67248d0b78e228feb03
Vishal Kumar <76913781+vishalr7667@users.noreply.github.com>
2024-10-05 10:45:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -425,4 +425,4 @@ def rounding(value, precision=2):
     return round(value, precision)
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
